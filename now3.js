07a75dd264ae325098b105bc5ffde3db08d51971
BrittanyRoll <artemismoon2015@gmail.com>
2017-08-21 15:41:04 -0400

    Update now3.js

--- now3.js
+++ now3.js
@@ -508,24 +508,24 @@ function weekFunction() {
     var time = prompt("Enter the time you would like to complete the task", "5:00");
     var task = prompt("Enter the task you would like ot complete", "write essay");
     if (day == ("monday")){
-      document.getElementById("monday").innerHTML = document.getElementById("monday").innerHTML + "<br>" + "on" + " " + day + " " + "at" + " " + time + " " + task;
+      document.getElementById("monday").innerHTML = document.getElementById("monday").innerHTML + "<br>" + " " + day + " " + "at" + " " + time + " " + task;
     }
-    if (day == ("tuesday")){
+    if (day == ("tuesday") || ("Tuesday")){
       document.getElementById("tuesday").innerHTML = document.getElementById("tuesday").innerHTML + "<br>" + "on" + " " + day + " " + "at" + " " + time + " " + task;
     }
-    if (day == ("wednesday")){
+    if (day == ("wednesday") || ("Wednesday")){
       document.getElementById("wednesday").innerHTML = document.getElementById("wednesday").innerHTML + "<br>" + "on" + " " + day + " " + "at" + " " + time + " " + task;
     }
-    if (day == ("thursday")){
+    if (day == ("thursday") || ("Thursday")){
       document.getElementById("thursday").innerHTML = document.getElementById("thursday").innerHTML + "<br>" + "on" + " " + day + " " + "at" + " " + time + " " + task;
     }
-    if (day == ("friday")){
+    if (day == ("friday") || ("Friday")){
       document.getElementById("friday").innerHTML = document.getElementById("friday").innerHTML + "<br>" + "on" + " " + day + " " + "at" + " " + time + " " + task;
     }
-    if (day == ("saturday")){
+    if (day == ("saturday") || ("saturday")){
       document.getElementById("saturday").innerHTML = document.getElementById("saturday").innerHTML + "<br>" + "on" + " " + day + " " + "at" + " " + time + " " + task;
     }
-    if (day == ("sunday")){
+    if (day == ("sunday") || ("Sunday")){
       document.getElementById("sunday").innerHTML = document.getElementById("sunday").innerHTML + "<br>" + "on" + " " + day + " " + "at" + " " + time + " " + task;
     }
 }
